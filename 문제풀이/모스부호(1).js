fda75b44ce7c5fa66a32885d4623972bb7ecbe02
유지영 <yj0zero@gmail.com>
2022-11-29 17:39:58 +0900

    feat: 프로그래머스 '모스부호(1)' 정답 제출

---
+++ 문제풀이/모스부호(1).js
@@ -0,0 +1,40 @@
+morse = {
+  ".-": "a",
+  "-...": "b",
+  "-.-.": "c",
+  "-..": "d",
+  ".": "e",
+  "..-.": "f",
+  "--.": "g",
+  "....": "h",
+  "..": "i",
+  ".---": "j",
+  "-.-": "k",
+  ".-..": "l",
+  "--": "m",
+  "-.": "n",
+  "---": "o",
+  ".--.": "p",
+  "--.-": "q",
+  ".-.": "r",
+  "...": "s",
+  "-": "t",
+  "..-": "u",
+  "...-": "v",
+  ".--": "w",
+  "-..-": "x",
+  "-.--": "y",
+  "--..": "z",
+};
+
+/**방법 1 */
+function solution(letter) {
+  let result = "";
+  letter.split(" ").forEach((item) => (result += morse[item]));
+  return result;
+}
+
+/**방법 2 */
+function solution(letter) {
+  return letter.split(" ").reduce((prev, curr) => prev + morse[curr], "");
+}
