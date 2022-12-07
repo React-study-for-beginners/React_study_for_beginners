18644b7dfeff2bee4ad80910d8361f24fad3c597
유지영 <yj0zero@gmail.com>
2022-12-07 12:59:12 +0900

    feat: 프로그래머스 저주의 숫자 3 정답 제출

---
+++ 문제풀이/저주의 숫자 3.js
@@ -0,0 +1,11 @@
+function solution(n) {
+  let num = 0;
+  for (let i = 1; i <= n; i++) {
+    num += 1;
+    while (num % 3 === 0 || String(num).includes("3")) {
+      num += 1;
+    }
+  }
+
+  return num;
+}
