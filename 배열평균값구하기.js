bc8a9194ab5fe424bdeb8f6f938ab22da230bfda
Dain Lee <108778921+feb-dain@users.noreply.github.com>
2022-12-15 12:19:03 +0900

    Revert "[째잉] 3주차 코딩테스트 문제풀이 PR (2)"

--- 배열평균값구하기.js
+++
@@ -1,8 +0,0 @@
-function solution(numbers) {
-    const array = [...numbers];
-    const average = array.reduce((a, b) => a + b) / array.length;
-    
-    return average;
-}
-
-// reduce 함수, 초기 값 전달 > 배열 요소들 순차적 전달, 총 합 도출
